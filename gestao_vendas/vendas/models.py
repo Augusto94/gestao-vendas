a186d5546192f7418708b28e46ca430a3102d040
augusto94 <arl4@cin.ufpe.br>
2018-08-10 21:14:48 -0300

    Cria model Cliente

--- gestao_vendas/vendas/models.py
+++ gestao_vendas/vendas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=30, blank=True)
+    apelido = models.CharField(max_length=30, blank=True)
+    idade = models.IntegerField(null=True, blank=True)
+    documento = models.CharField(max_length=20, blank=True, null=True)
+    telefone = models.CharField(max_length=13, blank=True, null=True)
+
+    def __str__(self):
+        return self.apelido
